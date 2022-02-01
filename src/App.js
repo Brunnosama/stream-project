3aafbf93d292d969be98507e262dae5fa125f638
Brunno Manoel Azevedo Pessôa <brunno.pessoa@al.infnet.edu.br>
2022-01-31 22:39:48 -0300

    header config

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
-import { Button } from "react-bootstrap";
+import { Container, Nav, Navbar } from "react-bootstrap"
+import Logo from './assets/img/assista-logo.png';
 
 function App() {
   return (
-    <div>
-      <p>Plataforma Assista</p>
-      <Button variant='primary'>Test</Button>
-    </div>
+    <Navbar expand="md" className='header-navbar'>
+  <Container>
+    <Navbar.Brand href="#home">
+      <img src={Logo} alt="Logo Assista"/>
+    </Navbar.Brand>
+    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+    <Navbar.Collapse id="basic-navbar-nav">
+      <Nav className="me-auto">
+        <Nav.Link href="#home">Home</Nav.Link>
+        <Nav.Link href="#catálogo">Video Library</Nav.Link>
+        <Nav.Link href="#perfil">Profile</Nav.Link>
+        </Nav>
+    </Navbar.Collapse>
+  </Container>
+</Navbar>
   );
 }
 
