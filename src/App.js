f291bb36d9b88b529464dc887a531f483d2e2f70
Brunno Manoel Azevedo Pessôa <brunno.pessoa@al.infnet.edu.br>
2022-02-01 17:29:26 -0300

    header config fix

--- src/App.js
+++ src/App.js
@@ -3,21 +3,23 @@ import Logo from './assets/img/assista-logo.png';
 
 function App() {
   return (
-    <Navbar expand="md" className='header-navbar'>
-  <Container>
-    <Navbar.Brand href="#home">
-      <img src={Logo} alt="Logo Assista"/>
-    </Navbar.Brand>
-    <Navbar.Toggle aria-controls="basic-navbar-nav" />
-    <Navbar.Collapse id="basic-navbar-nav">
-      <Nav className="me-auto">
-        <Nav.Link href="#home">Home</Nav.Link>
-        <Nav.Link href="#catálogo">Video Library</Nav.Link>
-        <Nav.Link href="#perfil">Profile</Nav.Link>
-        </Nav>
-    </Navbar.Collapse>
-  </Container>
-</Navbar>
+    <header>
+      <Navbar expand="md" className='header-navbar'>
+        <Container>
+          <Navbar.Brand href="#home">
+            <img src={Logo} alt="Logo Assista" height={50} width={60} />
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="me-auto">
+              <Nav.Link href="/">Home</Nav.Link>
+              <Nav.Link href="/library">Video Library</Nav.Link>
+              <Nav.Link href="/profile">Profile</Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </header>
   );
 }
 
