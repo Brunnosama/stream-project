851f517ee2e45c09e301ac27674f5fe0ca51b974
Brunno Manoel Azevedo Pessôa <brunno.pessoa@al.infnet.edu.br>
2022-02-01 22:09:43 -0300

    home v1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
+import React from 'react'
 import { Container, Nav, Navbar } from "react-bootstrap"
-import Logo from './assets/img/assista-logo.png';
+import Logo from './assets/img/assista-logo.png'
+import Banner from './assets/img/assista-banner.png'
 
 function App() {
   return (
-    <header>
-      <Navbar expand="md" className='header-navbar'>
+    <React.Fragment>
+      <header>
+        <Navbar expand="md" className='header-navbar'>
+          <Container>
+            <Navbar.Brand href="#home">
+              <img src={Logo} alt="Logo Assista" height={50} width={60} />
+            </Navbar.Brand>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Collapse id="basic-navbar-nav">
+              <Nav className="me-auto">
+                <Nav.Link href="/">Home</Nav.Link>
+                <Nav.Link href="/library">Video Library</Nav.Link>
+                <Nav.Link href="/profile">Profile</Nav.Link>
+              </Nav>
+            </Navbar.Collapse>
+          </Container>
+        </Navbar>
+      </header>
+      <main>
         <Container>
-          <Navbar.Brand href="#home">
-            <img src={Logo} alt="Logo Assista" height={50} width={60} />
-          </Navbar.Brand>
-          <Navbar.Toggle aria-controls="basic-navbar-nav" />
-          <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className="me-auto">
-              <Nav.Link href="/">Home</Nav.Link>
-              <Nav.Link href="/library">Video Library</Nav.Link>
-              <Nav.Link href="/profile">Profile</Nav.Link>
-            </Nav>
-          </Navbar.Collapse>
+          <div className='my-3 banner-home'>
+            <img src={Banner} alt='Assista Banner' className='img-fluid' height={202} width={333}/>
+            <h3>
+              The most complete <br/> Streaming Service out there.
+            </h3>
+          </div>
         </Container>
-      </Navbar>
-    </header>
+      </main>
+      <footer className='text-center footer'>
+        <p className='m-0'>All rights reserved to Assista™</p>
+      </footer>
+    </React.Fragment>
   );
 }
 
