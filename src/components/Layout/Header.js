dcdf54d4b4f404a1ed80d9fbeaee20356f5a15bc
Brunno Manoel Azevedo Pessôa <brunno.pessoa@al.infnet.edu.br>
2022-02-04 17:07:13 -0300

    href to link

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -1,20 +1,21 @@
 import {Navbar, Container, Nav} from 'react-bootstrap';
 import Logo from '../../assets/img/assista-logo.png';
+import { Link } from 'react-router-dom';
 
 export function Header() {
     return (
         <header>
             <Navbar expand="md" className='header-navbar'>
                 <Container>
-                    <Navbar.Brand href="#home">
+                    <Navbar.Brand as={Link} to="/">
                         <img src={Logo} alt="Logo Assista" height={50} width={60} />
                     </Navbar.Brand>
                     <Navbar.Toggle aria-controls="basic-navbar-nav" />
                     <Navbar.Collapse id="basic-navbar-nav">
                         <Nav className="me-auto">
-                            <Nav.Link href="/">Home</Nav.Link>
-                            <Nav.Link href="/library">Video Library</Nav.Link>
-                            <Nav.Link href="/profile">Profile</Nav.Link>
+                            <Nav.Link as={Link} to="/">Home</Nav.Link>
+                            <Nav.Link as={Link} to="/library">Video Library</Nav.Link>
+                            <Nav.Link as={Link} to="/profile">Profile</Nav.Link>
                         </Nav>
                     </Navbar.Collapse>
                 </Container>
--- src/views/NotFound/index.js
+++ src/views/NotFound/index.js
@@ -6,8 +6,8 @@ export function NotFoundView() {
         <Layout>
             <Container className ="text-center mt-4">
                 <h1>Page not Found</h1>
-                <p>The page your loking for is unavailable or was moved elsewere</p>
-                <p>Use the menu to navigate between pages</p>
+                <p>The page your loking for is unavailable or was moved elsewere.</p>
+                <p>Use the menu to navigate between pages.</p>
             </Container>
         </Layout>
     )
