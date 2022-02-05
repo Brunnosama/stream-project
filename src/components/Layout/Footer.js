16b405387da905bfb96cf49b980ad7f277a507b9
Brunno Manoel Azevedo Pessôa <brunno.pessoa@al.infnet.edu.br>
2022-02-04 22:09:46 -0300

    gradient bg fix

--- src/components/Layout/Footer.js
+++ src/components/Layout/Footer.js
@@ -1,7 +1,7 @@
 function Footer() {
     return (
         <footer className='text-center footer'>
-            <p className='m-0'>All rights reserved to Assista™</p>
+            <p className='m-1'>All rights reserved to Assista™</p>
         </footer>
     )
 }
