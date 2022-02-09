d326b37a471206c74afca23fc6abc81958dfca93
Brunno Manoel Azevedo Pessôa <brunno.pessoa@al.infnet.edu.br>
2022-02-08 22:18:42 -0300

    video details screen config

--- src/views/VideosDetails/FavoritesForm.js
+++ src/views/VideosDetails/FavoritesForm.js
@@ -3,30 +3,28 @@ import { Button, Form } from 'react-bootstrap';
 
 export function FavoritesForm() {
 
-    const [name, setName] = useState('')
+    const [favoriteList, setFavoriteList] = useState('')
+    const handleSubmit = (event) => {
+        event.preventDefault()
+
+    }
 
     return (
         <>
             <h2>Add to Favorites</h2>
-            <Form>
-                <Form.Group className="mb-3" controlId="formBasicFavorites">
-                    <Form.Label>Select the Favorite list</Form.Label>
-                    <Form.Control type="text" placeholder="Enter list name" />
+            <Form onSubmit={handleSubmit}>
+                <Form.Group className="mb-3" controlId="favorites-list">
+                    <Form.Label className="mb-0">Select the Favorite list</Form.Label>
+                    <Form.Control 
+                        type="text"
+                        placeholder="Enter list name"
+                        required
+                        value={favoriteList}
+                        onChange={(event)=> setFavoriteList(event.target.value)
+                        }/>
                 </Form.Group>
             </Form>
-            <Button variant="dark">Add Video</Button>
-            {/*             
-            <input
-                type='text'
-                placeholder="Name uncontrolled"
-            />
-            <input
-                type='text'
-                placeholder="Name controlled"
-                value={name}
-                onChange={(event) => setName(event.target.value)}
-            />
-            <p>{name}</p> */}
+            <Button className="mb-3" type="submit"variant="dark">Add Video</Button>
         </>
 
     );
