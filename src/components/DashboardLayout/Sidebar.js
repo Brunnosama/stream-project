38e3a2c0ffe1818ed8fdca8549a8f39c06aceaa8
Brunnosama <apbrunno@gmail.com>
2022-07-24 17:44:53 -0300

    feat: config sidebar filter by role

--- src/components/DashboardLayout/Sidebar.js
+++ src/components/DashboardLayout/Sidebar.js
@@ -1,30 +1,36 @@
 import { CloseButton, Nav } from "react-bootstrap";
+import { useSelector } from "react-redux";
 import styled from "styled-components";
 import { SidebarItem } from "./SidebarItem";
+import { selectUser } from "../../store/User/User.selectors"
 
 const menuItems = [
     {
         to: '/dashboard',
         text: 'Dashboard',
-        checkAllPath: true
+        checkAllPath: true,
+        userTypes: [1, 2]
     },
     {
         to: '/dashboard/videos',
         text: 'Videos',
-        checkAllPath: false
+        checkAllPath: false,
+        userTypes: [1]
     }
 ]
 
 export function Sidebar({ toClose, isOpen }) {
+    const user = useSelector(selectUser)
     return (
         <SidebarStyled isOpen={isOpen} className='text-white d-flex flex-column p-3'>
             <CloseButton onClick={toClose} variant="white" className='ms-auto d-lg-none' />
             <p className='h1'>Assista Service</p>
             <hr />
             <Nav variant="pills" className="flex-column">
-                {menuItems.map((item, index) => (
-                    <SidebarItem key={index} item={item} />
-                ))}
+                {menuItems.filter(item =>
+                    item.userTypes.includes(user.type)).map((item, index) => (
+                        <SidebarItem key={index} item={item} />
+                    ))}
             </Nav>
         </SidebarStyled>
     )
